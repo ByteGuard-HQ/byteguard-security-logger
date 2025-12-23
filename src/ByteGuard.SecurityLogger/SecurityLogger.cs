39e19a3e5429398356fcf04aa30b0ed7c23943c9
detilium <chrhaase@icloud.com>
2025-12-23 18:03:20 +0100

    feat: add upload events extensions

--- src/ByteGuard.SecurityLogger/SecurityLogger.cs
+++ src/ByteGuard.SecurityLogger/SecurityLogger.cs
@@ -741,10 +741,194 @@ public static class SecurityLoggerExtensions
         SecurityEventMetadata metadata,
         params object?[] args)
     {
-        var evt = $"{LoggingVocabulary.ExcessRateLimitExceeded}:{userId}:{max}";
+        var evt = $"{LoggingVocabulary.ExcessRateLimitExceeded}:{userId},{max}";
         metadata ??= new SecurityEventMetadata();
 
         Log(logger, evt, LogLevel.Warning, message, metadata, args);
+    }
+
+    /// <summary>
+    /// Upload completed.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="fileName">File name.</param>
+    /// <param name="fileType">File type.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUploadComplete(
+        this ILogger logger,
+        string message,
+        string userId,
+        string fileName,
+        string fileType,
+        params object?[] args)
+    {
+        logger.LogUploadComplete(message, userId, fileName, fileType, new SecurityEventMetadata(), args);
+    }
+
+    /// <summary>
+    /// Upload completed.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="fileName">File name.</param>
+    /// <param name="fileType">File type.</param>
+    /// <param name="metadata">Security event metadata.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUploadComplete(
+        this ILogger logger,
+        string message,
+        string userId,
+        string fileName,
+        string fileType,
+        SecurityEventMetadata metadata,
+        params object?[] args)
+    {
+        var evt = $"{LoggingVocabulary.UploadComplete}:{userId},{fileName},{fileType}";
+        metadata ??= new SecurityEventMetadata();
+
+        Log(logger, evt, LogLevel.Information, message, metadata, args);
+    }
+
+    /// <summary>
+    /// Upload stored.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="from">Original storage location.</param>
+    /// <param name="to">New storage location.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUploadStored(
+        this ILogger logger,
+        string message,
+        string userId,
+        string from,
+        string to,
+        params object?[] args)
+    {
+        logger.LogUploadStored(message, userId, from, to, new SecurityEventMetadata(), args);
+    }
+
+    /// <summary>
+    /// Upload stored.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="from">Original storage location.</param>
+    /// <param name="to">New storage location.</param>
+    /// <param name="metadata">Security event metadata.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUploadStored(
+        this ILogger logger,
+        string message,
+        string userId,
+        string from,
+        string to,
+        SecurityEventMetadata metadata,
+        params object?[] args)
+    {
+        var evt = $"{LoggingVocabulary.UploadStored}:{userId},{from},{to}";
+        metadata ??= new SecurityEventMetadata();
+
+        Log(logger, evt, LogLevel.Information, message, metadata, args);
+    }
+
+    /// <summary>
+    /// Upload validation.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="filename">File name.</param>
+    /// <param name="validationType">Validation type (e.g. virusscan, signature, size, etc.).</param>
+    /// <param name="result">Validation result (e.g. FAILED, incomplete, passed).</param>
+    /// <param name="level">Log level.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUploadValidation(
+        this ILogger logger,
+        string message,
+        string userId,
+        string filename,
+        string validationType,
+        string result,
+        LogLevel level,
+        params object?[] args)
+    {
+        logger.LogUploadValidation(message, userId, filename, validationType, result, level, new SecurityEventMetadata(), args);
+    }
+
+    /// <summary>
+    /// Upload validation.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="filename">File name.</param>
+    /// <param name="validationType">Validation type (e.g. virusscan, signature, size, etc.).</param>
+    /// <param name="result">Validation result (e.g. FAILED, incomplete, passed).</param>
+    /// <param name="level">Log level.</param>
+    /// <param name="metadata">Security event metadata.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUploadValidation(
+        this ILogger logger,
+        string message,
+        string userId,
+        string filename,
+        string validationType,
+        string result,
+        LogLevel level,
+        SecurityEventMetadata metadata,
+        params object?[] args)
+    {
+        var evt = $"{LoggingVocabulary.UploadValidation}:{userId},{filename},{validationType}:{result}";
+        metadata ??= new SecurityEventMetadata();
+
+        Log(logger, evt, level, message, metadata, args);
+    }
+
+    /// <summary>
+    /// Upload deleted.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="fileId">File identifier.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUploadDelete(
+        this ILogger logger,
+        string message,
+        string userId,
+        string fileId,
+        params object?[] args)
+    {
+        logger.LogUploadDelete(message, userId, fileId, new SecurityEventMetadata(), args);
+    }
+
+    /// <summary>
+    /// Upload deleted.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="fileId">File identifier.</param>
+    /// <param name="metadata">Security event metadata.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUploadDelete(
+        this ILogger logger,
+        string message,
+        string userId,
+        string fileId,
+        SecurityEventMetadata metadata,
+        params object?[] args)
+    {
+        var evt = $"{LoggingVocabulary.UploadDelete}:{userId},{fileId}";
+        metadata ??= new SecurityEventMetadata();
+
+        Log(logger, evt, LogLevel.Information, message, metadata, args);
     }
 
     /// <summary>
