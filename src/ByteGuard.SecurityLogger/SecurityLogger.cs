48495b63649152c51e413d3ef1f607e03e8f6ce2
detilium <chrhaase@icloud.com>
2025-12-23 21:30:12 +0100

    feat: add user events extension

--- src/ByteGuard.SecurityLogger/SecurityLogger.cs
+++ src/ByteGuard.SecurityLogger/SecurityLogger.cs
@@ -1867,6 +1867,182 @@ public static class SecurityLoggerExtensions
     }
 
     /// <summary>
+    /// User created.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="newUserId">New user identifier.</param>
+    /// <param name="attributes">New user attributes.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUserCreated(
+        this ILogger logger,
+        string message,
+        string userId,
+        string newUserId,
+        Dictionary<string, IEnumerable<string>> attributes,
+        params object?[] args)
+    {
+        logger.LogUserCreated(message, userId, newUserId, attributes, new SecurityEventMetadata(), args);
+    }
+
+    /// <summary>
+    /// User created.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="newUserId">New user identifier.</param>
+    /// <param name="attributes">New user attributes.</param>
+    /// <param name="metadata">Security event metadata.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUserCreated(
+        this ILogger logger,
+        string message,
+        string userId,
+        string newUserId,
+        Dictionary<string, IEnumerable<string>> attributes,
+        SecurityEventMetadata metadata,
+        params object?[] args)
+    {
+        var commaSeparatedAttributes = string.Join(",", attributes.Select(kvp => $"{kvp.Key}:{string.Join(",", kvp.Value)}"));
+
+        var evt = $"{LoggingVocabulary.UserCreated}:{userId},{newUserId},{commaSeparatedAttributes}";
+        metadata ??= new SecurityEventMetadata();
+
+        Log(logger, evt, LogLevel.Warning, message, metadata, args);
+    }
+
+    /// <summary>
+    /// User updated.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="onUserId">On user identifier.</param>
+    /// <param name="attributes">User attributes.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUserUpdated(
+        this ILogger logger,
+        string message,
+        string userId,
+        string onUserId,
+        Dictionary<string, IEnumerable<string>> attributes,
+        params object?[] args)
+    {
+        logger.LogUserUpdated(message, userId, onUserId, attributes, new SecurityEventMetadata(), args);
+    }
+
+    /// <summary>
+    /// User updated.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="onUserId">On user identifier.</param>
+    /// <param name="attributes">User attributes.</param>
+    /// <param name="metadata">Security event metadata.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUserUpdated(
+        this ILogger logger,
+        string message,
+        string userId,
+        string onUserId,
+        Dictionary<string, IEnumerable<string>> attributes,
+        SecurityEventMetadata metadata,
+        params object?[] args)
+    {
+        var commaSeparatedAttributes = string.Join(",", attributes.Select(kvp => $"{kvp.Key}:{string.Join(",", kvp.Value)}"));
+
+        var evt = $"{LoggingVocabulary.UserUpdated}:{userId},{onUserId},{commaSeparatedAttributes}";
+        metadata ??= new SecurityEventMetadata();
+
+        Log(logger, evt, LogLevel.Warning, message, metadata, args);
+    }
+
+    /// <summary>
+    /// User archived.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="onUserId">On user identifier.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUserArchived(
+        this ILogger logger,
+        string message,
+        string userId,
+        string onUserId,
+        params object?[] args)
+    {
+        logger.LogUserArchived(message, userId, onUserId, new SecurityEventMetadata(), args);
+    }
+
+    /// <summary>
+    /// User archived.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="onUserId">On user identifier.</param>
+    /// <param name="metadata">Security event metadata.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUserArchived(
+        this ILogger logger,
+        string message,
+        string userId,
+        string onUserId,
+        SecurityEventMetadata metadata,
+        params object?[] args)
+    {
+        var evt = $"{LoggingVocabulary.UserArchived}:{userId},{onUserId}";
+        metadata ??= new SecurityEventMetadata();
+
+        Log(logger, evt, LogLevel.Warning, message, metadata, args);
+    }
+
+    /// <summary>
+    /// User deleted.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="onUserId">On user identifier.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUserDeleted(
+        this ILogger logger,
+        string message,
+        string userId,
+        string onUserId,
+        params object?[] args)
+    {
+        logger.LogUserDeleted(message, userId, onUserId, new SecurityEventMetadata(), args);
+    }
+
+    /// <summary>
+    /// User deleted.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="onUserId">On user identifier.</param>
+    /// <param name="metadata">Security event metadata.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogUserDeleted(
+        this ILogger logger,
+        string message,
+        string userId,
+        string onUserId,
+        SecurityEventMetadata metadata,
+        params object?[] args)
+    {
+        var evt = $"{LoggingVocabulary.UserDeleted}:{userId},{onUserId}";
+        metadata ??= new SecurityEventMetadata();
+
+        Log(logger, evt, LogLevel.Warning, message, metadata, args);
+    }
+
+    /// <summary>
     /// Generic log method.
     /// </summary>
     /// <param name="logger">ILogger implementation.</param>
