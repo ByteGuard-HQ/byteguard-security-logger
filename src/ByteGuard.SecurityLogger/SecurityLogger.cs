7aa35f84a317e6416ad17c9a8ac8e27f8f34ef1e
detilium <chrhaase@icloud.com>
2025-12-23 17:45:25 +0100

    feat: add excess events extensions

--- src/ByteGuard.SecurityLogger/SecurityLogger.cs
+++ src/ByteGuard.SecurityLogger/SecurityLogger.cs
@@ -707,6 +707,47 @@ public static class SecurityLoggerExtensions
     }
 
     /// <summary>
+    /// Rate limit exceeded.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="max">Maximum allowed requests.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogExcessRateLimitExceeded(
+        this ILogger logger,
+        string message,
+        string userId,
+        int max,
+        params object?[] args)
+    {
+        logger.LogExcessRateLimitExceeded(message, userId, max, new SecurityEventMetadata(), args);
+    }
+
+    /// <summary>
+    /// Rate limit exceeded.
+    /// </summary>
+    /// <param name="logger">ILogger implementation.</param>
+    /// <param name="message">Log message.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="max">Maximum allowed requests.</param>
+    /// <param name="metadata">Security event metadata.</param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public static void LogExcessRateLimitExceeded(
+        this ILogger logger,
+        string message,
+        string userId,
+        int max,
+        SecurityEventMetadata metadata,
+        params object?[] args)
+    {
+        var evt = $"{LoggingVocabulary.ExcessRateLimitExceeded}:{userId}:{max}";
+        metadata ??= new SecurityEventMetadata();
+
+        Log(logger, evt, LogLevel.Warning, message, metadata, args);
+    }
+
+    /// <summary>
     /// Generic log method.
     /// </summary>
     /// <param name="logger">ILogger implementation.</param>
